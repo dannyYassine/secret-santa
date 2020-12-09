470ce4210aeaa9d3f6522fa216579f15189df273
Danny <dannyyassine@gmail.com>
2020-12-09 15:00:15 -0500

    no message

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,11 +3,10 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git remote add heroku https://${ENV_HEROKU_USERNAME}:${ENV_HEROKU_PASSWORD}@git.heroku.com/dannyyassine-secret-santa.git > /dev/null 2>&1
 }
 
 deploy() {
-  git subtree push --prefix api heroku master
+  git subtree push --prefix api https://${ENV_HEROKU_USERNAME}:${ENV_HEROKU_PASSWORD}@git.heroku.com/dannyyassine-secret-santa.git
 }
 
 setup_git
